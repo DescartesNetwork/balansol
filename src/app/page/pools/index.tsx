c9c7796cbe2405930e77fd364eea233594228749
ngduytradescartes <95331134+ngduytradescartes@users.noreply.github.com>
2022-03-11 15:46:25 +0700

    folder structure

--- src/app/page/pools/index.tsx
+++ src/app/page/pools/index.tsx
@@ -1,5 +1,35 @@
 import React from 'react'
 
+import { Card, Col, Row } from 'antd'
+import InfoCard from './InfoCard'
+import New from './new'
+import Search from './search'
+
 export default function Pools() {
-  return <div>Pools</div>
+  return (
+    <Row gutter={[24, 24]}>
+      <Col span={24}>
+        <Row>
+          <Col>
+            <Search />
+          </Col>
+          <Col>
+            <New />
+          </Col>
+        </Row>
+      </Col>
+
+      <Col span={24}>
+        <Row>
+          {[1, 2, 3].map((value) => {
+            return (
+              <Col span={12}>
+                <InfoCard />
+              </Col>
+            )
+          })}
+        </Row>
+      </Col>
+    </Row>
+  )
 }
--- src/app/page/swap/review/index.tsx
+++ src/app/page/swap/review/index.tsx
@@ -1,5 +1,29 @@
-import React from 'react'
+import { Button, Card, Col, Row, Typography } from 'antd'
+import React, { useState } from 'react'
+import ConfirmSwap from './confirmSwap'
 
 export default function Review() {
-  return <div>Review</div>
+  const [visible, setVisivle] = useState(false)
+  return (
+    <Card bordered={false}>
+      <Row gutter={[24, 24]}>
+        <Col span={24}>
+          <Button
+            type="primary"
+            onClick={() => setVisivle(true)}
+            disabled={false}
+            block
+          >
+            Review
+          </Button>
+        </Col>
+        <ConfirmSwap
+          visible={visible}
+          onCancel={() => {
+            setVisivle(false)
+          }}
+        />
+      </Row>
+    </Card>
+  )
 }
