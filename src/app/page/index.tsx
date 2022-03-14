2afb08689d9b6f0e4d795d3ed62b5ce3fe83c3c8
ngduytradescartes <95331134+ngduytradescartes@users.noreply.github.com>
2022-03-14 13:03:27 +0700

    format css

--- src/app/page/index.tsx
+++ src/app/page/index.tsx
@@ -14,45 +14,27 @@ const Page = () => {
 
   return (
     <AppWatcher>
-      <Row
-        gutter={[24, 24]}
-        align="middle"
-        className="balancer"
-        justify="center"
-      >
-        <Col xs={24} md={12} lg={8}>
-          <Row gutter={[24, 24]}>
-            <Col span={24}>
-              <Radio.Group
-                onChange={(val) => setWrapTab(val.target.value)}
-                className="pool-option"
-                disabled={false}
-                value={wrapTab}
-              >
-                <Radio.Button value={WrapTabs.Wrap}>Swap</Radio.Button>
-                <Radio.Button value={WrapTabs.Pools}>Pools</Radio.Button>
-              </Radio.Group>
-            </Col>
-            <Col span={24}>
-              <Tabs
-                activeKey={wrapTab}
-                centered
-                style={{
-                  boxShadow:
-                    wrapTab !== WrapTabs.Pools
-                      ? '0px 4px 44px rgba(0, 0, 0, 0.42)'
-                      : 'unset',
-                }}
-              >
-                <Tabs.TabPane key={WrapTabs.Wrap}>
-                  <Swap />
-                </Tabs.TabPane>
-                <Tabs.TabPane key={WrapTabs.Pools}>
-                  <Pools />
-                </Tabs.TabPane>
-              </Tabs>
-            </Col>
-          </Row>
+      <Row gutter={[24, 24]}>
+        <Col span={24} style={{ display: 'flex', justifyContent: 'center' }}>
+          <Radio.Group
+            onChange={(val) => setWrapTab(val.target.value)}
+            className="pool-option"
+            disabled={false}
+            value={wrapTab}
+          >
+            <Radio.Button value={WrapTabs.Wrap}>Swap</Radio.Button>
+            <Radio.Button value={WrapTabs.Pools}>Pools</Radio.Button>
+          </Radio.Group>
+        </Col>
+        <Col span={24}>
+          <Tabs activeKey={wrapTab} centered className="swap-tab">
+            <Tabs.TabPane key={WrapTabs.Wrap}>
+              <Swap />
+            </Tabs.TabPane>
+            <Tabs.TabPane key={WrapTabs.Pools}>
+              <Pools />
+            </Tabs.TabPane>
+          </Tabs>
         </Col>
       </Row>
     </AppWatcher>
