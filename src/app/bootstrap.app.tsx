476a117d727ee4d2a6c4711d4f58f187dcd47b91
The Vinh <vinhnguyen@descartes.network>
2022-03-15 16:04:30 +0700

    create UI

--- src/app/bootstrap.app.tsx
+++ src/app/bootstrap.app.tsx
@@ -13,6 +13,7 @@ import WidgetView from 'app/widget'
 import model from 'app/model'
 import configs from 'app/configs'
 
+import 'app/static/styles/index.less'
 const {
   manifest: { appId },
 } = configs
--- src/app/page/poolDetails/deposit.tsx
+++ src/app/page/poolDetails/deposit.tsx
@@ -2,10 +2,12 @@ import { Fragment, useState } from 'react'
 import { useSelector } from 'react-redux'
 import { BN } from '@project-serum/anchor'
 
-import { Button, Input, Modal } from 'antd'
+import { Button, Col, Modal, Row, Typography } from 'antd'
 
 import { notifyError, notifySuccess } from 'app/helper'
 import { AppState } from 'app/model'
+import IonIcon from 'shared/antd/ionicon'
+import CardToken from './cardToken'
 
 const Deposit = ({ poolAddress }: { poolAddress: string }) => {
   const [visible, setVisible] = useState(false)
@@ -40,19 +42,69 @@ const Deposit = ({ poolAddress }: { poolAddress: string }) => {
       <Button onClick={() => setVisible(true)}>Deposit</Button>
       {/* Modal deposit */}
       <Modal
-        title="Basic Modal"
+        title={<Typography.Title level={4}>Deposit</Typography.Title>}
         visible={visible}
         onCancel={() => setVisible(false)}
+        className="modal-balansol"
+        footer={null}
+        destroyOnClose={true}
+        centered={true}
+        closeIcon={<IonIcon name="close-outline" />}
       >
-        {poolData.mints.map((mint) => {
-          return (
-            <Input
-              onChange={(e) => onChange(mint.toBase58(), e.target.value)}
-              key={mint.toBase58()}
-            />
-          )
-        })}
-        <Button onClick={onSubmit}>Submit</Button>
+        <Row gutter={[0, 16]}>
+          <Col span={24}>
+            <Row gutter={[24, 8]}>
+              {poolData.mints.map((mint) => {
+                return (
+                  <Col span={24}>
+                    <CardToken mintAddress={mint.toBase58()}></CardToken>
+                  </Col>
+                )
+              })}
+            </Row>
+          </Col>
+          <Col span={24}>
+            <Row gutter={[0, 13]}>
+              <Col span={24}>
+                <Row align="middle">
+                  <Col flex="auto">
+                    <Typography.Text type="secondary">
+                      Price impact
+                    </Typography.Text>
+                  </Col>
+                  <Col>
+                    <span style={{ color: '#03A326' }}>0 %</span>
+                  </Col>
+                </Row>
+              </Col>
+              <Col span={24}>
+                <Row align="middle">
+                  <Col flex="auto">
+                    <Typography.Text type="secondary">
+                      You will reveice
+                    </Typography.Text>
+                  </Col>
+                  <Col>
+                    <Typography.Title level={4}>22.332 LP</Typography.Title>
+                  </Col>
+                </Row>
+              </Col>
+            </Row>
+          </Col>
+          <Col span={24}>
+            <Button
+              style={{
+                background: '#63E0B3',
+                color: '#081438',
+                borderRadius: '24px',
+              }}
+              type="primary"
+              block
+            >
+              Deposit
+            </Button>
+          </Col>
+        </Row>
       </Modal>
     </Fragment>
   )
