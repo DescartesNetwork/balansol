52d566542d344df0fa690a3bb7ffd67597fcb6ce
thiennv <thiennguyen@descartes.network>
2022-03-16 17:43:11 +0700

    update theme

--- src/app/bootstrap.app.tsx
+++ src/app/bootstrap.app.tsx
@@ -13,7 +13,8 @@ import WidgetView from 'app/widget'
 import model from 'app/model'
 import configs from 'app/configs'
 
-import 'app/static/styles/index.less'
+import 'app/static/styles/light.less'
+import 'app/static/styles/dark.less'
 
 const {
   manifest: { appId },
@@ -21,7 +22,7 @@ const {
 
 export const Page = () => {
   return (
-    <UIProvider appId={appId} antd>
+    <UIProvider appId={appId} antd={{ prefixCls: appId }}>
       <WalletProvider>
         <AccountProvider>
           <MintProvider>
@@ -44,7 +45,7 @@ export const widgetConfig: WidgetConfig = {
 
 export const Widget = () => {
   return (
-    <UIProvider appId={appId} antd>
+    <UIProvider appId={appId} antd={{ prefixCls: appId }}>
       <WalletProvider>
         <AccountProvider>
           <MintProvider>
--- src/app/page/swap/review/index.tsx
+++ src/app/page/swap/review/index.tsx
@@ -9,18 +9,7 @@ export default function Review() {
   return (
     <Row gutter={[24, 24]} style={{ width: '100%' }}>
       <Col span={24} style={{ padding: 0 }}>
-        <Button
-          type="primary"
-          onClick={() => setVisivle(true)}
-          disabled={false}
-          style={{
-            width: '100%',
-            background: 'linear-gradient(81.55deg, #F148FB 0%, #4E6ABE 100%)',
-            borderRadius: 40,
-            borderColor: 'transparent',
-          }}
-          block
-        >
+        <Button type="primary" onClick={() => setVisivle(true)} block>
           Review
         </Button>
       </Col>
