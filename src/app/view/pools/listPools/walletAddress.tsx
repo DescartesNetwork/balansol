114115e4c6b34e599de8ea2266490d670de2ac5a
The Vinh <vinhnguyen@descartes.network>
2022-04-13 14:52:41 +0700

    Add icon check owner pool

--- src/app/view/pools/listPools/walletAddress.tsx
+++ src/app/view/pools/listPools/walletAddress.tsx
@@ -1,13 +1,23 @@
-import React, { useState } from 'react'
+import { useState } from 'react'
+import { useSelector } from 'react-redux'
+import { useWallet } from '@senhub/providers'
 
 import { Space, Tooltip, Typography } from 'antd'
 import IconButton from 'os/view/actionCenter/applications/walletIntro/iconButton'
 import CopyToClipboard from 'react-copy-to-clipboard'
 
 import { explorer, shortenAddress } from 'shared/util'
+import IonIcon from 'shared/antd/ionicon'
+import { AppState } from 'app/model'
 
 const WalletAddress = ({ poolAddress }: { poolAddress: string }) => {
   const [copied, setCopied] = useState(false)
+  const {
+    pools: { [poolAddress]: poolData },
+  } = useSelector((state: AppState) => state)
+  const {
+    wallet: { address: walletAddress },
+  } = useWallet()
 
   const onCopy = async () => {
     setCopied(true)
@@ -17,6 +27,11 @@ const WalletAddress = ({ poolAddress }: { poolAddress: string }) => {
   }
   return (
     <Space size={10}>
+      {poolData.authority.toBase58() === walletAddress && (
+        <Tooltip title="Your pool!">
+          <IonIcon name="person-outline" />
+        </Tooltip>
+      )}
       <Typography.Text
         type="secondary"
         style={{ cursor: 'pointer' }}
