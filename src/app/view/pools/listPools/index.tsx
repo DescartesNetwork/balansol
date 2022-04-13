88132473c371a29ae55f803e464ae2045feecb60
The Vinh <vinhnguyen@descartes.network>
2022-04-13 14:35:04 +0700

    handle not show pools are uninitialized

--- src/app/view/pools/listPools/index.tsx
+++ src/app/view/pools/listPools/index.tsx
@@ -1,18 +1,23 @@
 import { Col, Row } from 'antd'
-import DetailsCard from './detailsCard'
 import { useFilterPools } from 'app/hooks/pools/useFilterPools'
 import { useSearchPools } from 'app/hooks/pools/useSearchPools'
+import DetailsCard from './detailsCard'
 
 const ListPools = () => {
   const { poolsFilter } = useFilterPools()
   const listPools = useSearchPools(poolsFilter)
+
   return (
     <Row gutter={[24, 24]}>
-      {Object.keys(listPools).map((poolAddress) => (
-        <Col xs={24} md={12} key={poolAddress}>
-          <DetailsCard poolAddress={poolAddress} />
-        </Col>
-      ))}
+      {Object.keys(listPools).map((poolAddress) => {
+        let poolState: any = listPools[poolAddress].state
+        if (poolState['uninitialized']) return null
+        return (
+          <Col xs={24} md={12} key={poolAddress}>
+            <DetailsCard poolAddress={poolAddress} />
+          </Col>
+        )
+      })}
     </Row>
   )
 }
