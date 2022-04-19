87f94dc0a45a728c4d177395f5e8f55dabf60ab5
ngduytradescartes <95331134+ngduytradescartes@users.noreply.github.com>
2022-04-19 15:07:25 +0700

    temp

--- src/app/view/poolDetails/volume24h.tsx
+++ src/app/view/poolDetails/volume24h.tsx
@@ -1,42 +1,11 @@
-import React, { useCallback, useState } from 'react'
-import moment from 'moment'
+import { useState } from 'react'
 
 import BarChart from './charts/barChart'
 
-import { DataLoader } from 'shared/dataloader'
-import PoolService from 'app/stat/logic/pool/pool'
-
-const TTL_5_MIN = 300000
 export type VolumeData = { data: number; label: string }
 
 const Volume24h = ({ poolAddress }: { poolAddress: string }) => {
   const [volumes24h, setVolume24h] = useState<VolumeData[]>([])
-  const [loading, setLoading] = useState(false)
-
-  const fetchChart = useCallback(async () => {
-    if (!poolAddress) return
-    try {
-      setLoading(true)
-      const poolService = new PoolService(poolAddress)
-      const poolStat = await DataLoader.load(
-        'getDailyInfo' + poolAddress,
-        poolService.getDailyInfo,
-        { cache: { ttl: TTL_5_MIN } },
-      )
-      const volume24Data = Object.keys(poolStat).map((time) => {
-        return {
-          data: poolStat[time].volume,
-          label: moment(time, 'YYYYMMDD').format('MM/DD'),
-        }
-      })
-      setVolume24h(volume24Data)
-    } catch (error) {
-    } finally {
-      setLoading(false)
-    }
-  }, [poolAddress])
-
-  console.log(volumes24h, 'volumes24h')
 
   return <BarChart data={volumes24h} />
 }
