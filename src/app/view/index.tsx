141a3eeaf5fa2556e880a8a5965a7797326d638b
tuphan-dn <tuphan@descartes.network>
2022-05-04 13:25:16 +0700

    debug

--- src/app/view/index.tsx
+++ src/app/view/index.tsx
@@ -1,4 +1,4 @@
-import { Fragment, useCallback, useEffect } from 'react'
+import { useCallback, useEffect } from 'react'
 import { Route, Switch } from 'react-router-dom'
 import { useUI } from '@senhub/providers'
 
@@ -32,34 +32,32 @@ const SwapAndPools = () => {
   if (!selectedTab) return null
 
   return (
-    <Fragment>
-      <Row gutter={[24, 24]} justify="center" style={{ paddingBottom: 12 }}>
-        <Col>
-          <Segmented
-            options={Object.keys(HOMEPAGE_TABS).map((key) => {
-              return { label: key, value: HOMEPAGE_TABS[key] }
-            })}
-            value={selectedTab}
-            onChange={(val) => onChange(val.toString())}
-            block
-          />
-        </Col>
-        <Col span={24}>
-          <Tabs activeKey={selectedTab} centered className="swap-tab">
-            <Tabs.TabPane key={HOMEPAGE_TABS.Swap}>
-              <Swap />
-            </Tabs.TabPane>
-            <Tabs.TabPane key={HOMEPAGE_TABS.Pools}>
-              <Pools />
-            </Tabs.TabPane>
-          </Tabs>
-        </Col>
-      </Row>
-    </Fragment>
+    <Row gutter={[24, 24]} justify="center" style={{ paddingBottom: 12 }}>
+      <Col>
+        <Segmented
+          options={Object.keys(HOMEPAGE_TABS).map((key) => {
+            return { label: key, value: HOMEPAGE_TABS[key] }
+          })}
+          value={selectedTab}
+          onChange={(val) => onChange(val.toString())}
+          block
+        />
+      </Col>
+      <Col span={24}>
+        <Tabs activeKey={selectedTab} centered className="swap-tab">
+          <Tabs.TabPane key={HOMEPAGE_TABS.Swap}>
+            <Swap />
+          </Tabs.TabPane>
+          <Tabs.TabPane key={HOMEPAGE_TABS.Pools}>
+            <Pools />
+          </Tabs.TabPane>
+        </Tabs>
+      </Col>
+    </Row>
   )
 }
 
-const Page = () => {
+const View = () => {
   const { appRoute } = useAppRouter()
   const { setBackground } = useUI()
 
@@ -68,15 +66,13 @@ const Page = () => {
   }, [setBackground])
 
   return (
-    <div className="balansol-background">
-      <AppWatcher>
-        <Switch>
-          <Route exact path={appRoute} component={SwapAndPools} />
-          <Route path={`${appRoute}/details`} component={PoolDetails} />
-        </Switch>
-      </AppWatcher>
-    </div>
+    <AppWatcher>
+      <Switch>
+        <Route exact path={appRoute} component={SwapAndPools} />
+        <Route path={`${appRoute}/details`} component={PoolDetails} />
+      </Switch>
+    </AppWatcher>
   )
 }
 
-export default Page
+export default View
