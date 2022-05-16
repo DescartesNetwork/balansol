a1fd0c2fc3081f9aeecbfc92acf0193b5c67b6a2
ngduytradescartes <95331134+ngduytradescartes@users.noreply.github.com>
2022-05-17 00:51:05 +0700

    .

--- src/app/view/pools/newPool/index.tsx
+++ src/app/view/pools/newPool/index.tsx
@@ -1,8 +1,12 @@
-import { Fragment, useState } from 'react'
+import { Fragment, useCallback, useEffect, useState } from 'react'
+import { useSelector } from 'react-redux'
 
 import { Button, Modal, Typography } from 'antd'
 import IonIcon from 'shared/antd/ionicon'
 import ModalNewPool from './modalNewPool'
+import { AppState } from 'app/model'
+import { PoolState } from '@senswap/balancer'
+import { useWallet } from '@senhub/providers'
 
 export type TokenInfo = {
   addressToken: string
@@ -11,17 +15,35 @@ export type TokenInfo = {
 }
 
 const NewPool = () => {
+  const { pools } = useSelector((state: AppState) => state)
   const [visible, setVisible] = useState(false)
+  const [isInProcess, setIsInProcess] = useState(false)
+  const { wallet } = useWallet()
+
+  const checkUninitializePool = useCallback(async () => {
+    for (const poolAddress in pools) {
+      const poolData = pools[poolAddress]
+      if (poolData.authority.toBase58() !== wallet.address) continue
+      if (!(poolData.state as PoolState)['uninitialized']) continue
+      return setIsInProcess(true)
+    }
+    return setIsInProcess(false)
+  }, [pools, wallet.address])
+
+  useEffect(() => {
+    checkUninitializePool()
+  }, [checkUninitializePool])
+
   return (
     <Fragment>
       <Button
         className="btn-outline"
-        icon={<IonIcon name="add-outline" />}
+        icon={<IonIcon name={!isInProcess ? 'add-outline' : 'sync-outline'} />}
         onClick={() => setVisible(true)}
         style={{ borderRadius: 40 }}
         block
       >
-        New Pool
+        {!isInProcess ? 'New Pool' : 'Resume'}
       </Button>
       <Modal
         title={<Typography.Title level={4}>New Pool</Typography.Title>}
