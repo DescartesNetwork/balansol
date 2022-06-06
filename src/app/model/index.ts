ddae7c839b6fae2398648ae0ec8179d751340335
minhcong <83624415+dmcong@users.noreply.github.com>
2022-06-06 09:39:24 +0700

    Update index.ts

--- src/app/model/index.ts
+++ src/app/model/index.ts
@@ -1,5 +1,5 @@
 import { configureStore } from '@reduxjs/toolkit'
-import { devTools } from 'shared/devTools'
+import { devTools } from 'app/model/devTools'
 
 import main from 'app/model/main.controller'
 import pools from 'app/model/pools.controller'
