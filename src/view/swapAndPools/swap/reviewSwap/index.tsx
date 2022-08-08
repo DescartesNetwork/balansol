45cbb276e3b571001efe1777d2a3e590c6045e68
ngduytradescartes <95331134+ngduytradescartes@users.noreply.github.com>
2022-08-08 15:57:29 +0700

    Add error message when swapping

--- src/view/swapAndPools/swap/reviewSwap/index.tsx
+++ src/view/swapAndPools/swap/reviewSwap/index.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useMemo, useState } from 'react'
 import { useSelector } from 'react-redux'
 
 import { Button, Col, Row } from 'antd'
@@ -6,19 +6,27 @@ import ConfirmSwap from './confirmSwap'
 
 import { AppState } from 'model'
 import { useSwap } from 'hooks/useSwap'
+import { useWrapAccountBalance } from 'hooks/useWrapAccountBalance'
 
 import './index.less'
 
 const ReviewSwap = () => {
   const bidAmount = useSelector((state: AppState) => state.swap.bidAmount)
   const askAmount = useSelector((state: AppState) => state.swap.askAmount)
+  const bidMint = useSelector((state: AppState) => state.swap.bidMint)
   const loading = useSelector((state: AppState) => state.swap.loading)
 
   const [visible, setVisible] = useState(false)
   const { route } = useSwap()
+  const { balance } = useWrapAccountBalance(bidMint)
 
-  const disabled =
-    !Number(bidAmount) || !Number(askAmount) || !route.length || loading
+  const errorMessage = useMemo(() => {
+    if (!Number(bidAmount)) return 'Enter an amount'
+    if (balance < Number(bidAmount)) return 'Balance not enough'
+    if (!Number(askAmount) || !route.length) return 'Route not found'
+
+    return ''
+  }, [askAmount, balance, bidAmount, route.length])
 
   return (
     <Row gutter={[24, 24]}>
@@ -26,12 +34,12 @@ const ReviewSwap = () => {
         <Button
           type="primary"
           onClick={() => setVisible(true)}
-          disabled={disabled}
+          disabled={!!errorMessage}
           loading={loading && Number(bidAmount) + Number(askAmount) !== 0}
           block
           size="large"
         >
-          {!disabled ? 'Review' : 'Enter an amount'}
+          {!errorMessage ? 'Review' : errorMessage}
         </Button>
       </Col>
       <ConfirmSwap visible={visible} onCancel={() => setVisible(false)} />
