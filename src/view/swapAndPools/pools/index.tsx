51b230158e253c4a003725ac71ffec9eef7623a9
ngduytradescartes <95331134+ngduytradescartes@users.noreply.github.com>
2022-08-10 10:48:29 +0700

    alter fragment to Row and Col

--- src/view/swapAndPools/pools/index.tsx
+++ src/view/swapAndPools/pools/index.tsx
@@ -15,8 +15,10 @@ const Pools = () => {
   return (
     <Row gutter={[12, 12]} justify="center" align="middle">
       <Col xs={24} md={20} lg={16} xl={12} xxl={8}>
-        <Row gutter={[12, 12]} className="search-pools" justify="space-between">
-          <Search />
+        <Row gutter={[12, 12]} justify="space-between">
+          <Col flex="auto">
+            <Search />
+          </Col>
           <Col span={isMobile ? 24 : undefined}>
             <New />
           </Col>
