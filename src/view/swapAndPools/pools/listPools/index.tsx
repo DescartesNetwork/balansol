9ea0c77ea4bc087c8efa5de4f15f417226ed74dd
thiennv <thiennguyen@descartes.network>
2022-09-14 16:33:50 +0700

    sort by tvl + apy/2

--- src/view/swapAndPools/pools/listPools/index.tsx
+++ src/view/swapAndPools/pools/listPools/index.tsx
@@ -1,18 +1,44 @@
+import { useCallback, useEffect, useMemo, useState } from 'react'
 import { PoolState } from '@senswap/balancer'
 import LazyLoad from '@sentre/react-lazyload'
 
 import { Col, Row } from 'antd'
-import { useFilterPools } from 'hooks/pools/useFilterPools'
-import { useSearchedPools } from 'hooks/pools/useSearchedPools'
 import DetailsCard from './detailsCard'
 
+import { useFilterPools } from 'hooks/pools/useFilterPools'
+import { useSearchedPools } from 'hooks/pools/useSearchedPools'
+import { useTVL } from 'hooks/useTVL'
+
 const ListPools = () => {
+  const { getTVL } = useTVL()
   const { poolsFilter } = useFilterPools()
   const listPools = useSearchedPools(poolsFilter)
+  const pools = useMemo(() => Object.keys(listPools), [listPools])
+  const [filteredPools, setFilteredPools] = useState(pools)
+
+  const onFilteredPools = useCallback(async () => {
+    if (!pools.length) return setFilteredPools(pools)
+    const nextPools = await Promise.all(
+      Object.keys(listPools).map(async (poolAddress) => {
+        const tvl = await getTVL(listPools[poolAddress])
+        return {
+          poolAddress,
+          tvl,
+        }
+      }),
+    )
+    nextPools.sort((poolA, poolB) => poolB.tvl - poolA.tvl)
+    const result = nextPools.map(({ poolAddress }) => poolAddress)
+    return setFilteredPools(result)
+  }, [getTVL, listPools, pools])
+
+  useEffect(() => {
+    onFilteredPools()
+  }, [onFilteredPools])
 
   return (
     <Row gutter={[24, 24]}>
-      {Object.keys(listPools).map((poolAddress) => {
+      {filteredPools.map((poolAddress) => {
         const poolData = listPools[poolAddress]
         let poolState: PoolState = poolData.state
         if (poolState['uninitialized'] || poolState['deleted']) return null
