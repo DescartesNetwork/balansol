8cd0ac89a7026be7162122d605cca3d378622e97
minhcong <83624415+dmcong@users.noreply.github.com>
2022-10-04 13:44:02 +0700

    remove sort pool

--- src/view/pools/listPools/index.tsx
+++ src/view/pools/listPools/index.tsx
@@ -1,59 +1,20 @@
-import { Fragment, useCallback, useEffect, useMemo, useState } from 'react'
+import { Fragment } from 'react'
 import { PoolState } from '@senswap/balancer'
 import LazyLoad from '@sentre/react-lazyload'
 
-import { Col, Row, Spin } from 'antd'
+import { Col, Row } from 'antd'
 import DetailsCard from './detailsCard'
 
 import { useFilterPools } from 'hooks/pools/useFilterPools'
 import { useSearchedPools } from 'hooks/pools/useSearchedPools'
-import { useTVL } from 'hooks/useTVL'
 
 const ListPools = () => {
-  const { getTVL } = useTVL()
   const { poolsFilter } = useFilterPools()
   const listPools = useSearchedPools(poolsFilter)
-  const pools = useMemo(() => Object.keys(listPools), [listPools])
-  const [sortedPools, setSortedPools] = useState(pools)
-  const [loading, setLoading] = useState(false)
-
-  const onSortedPools = useCallback(async () => {
-    try {
-      setLoading(true)
-      if (!pools.length) return setSortedPools(pools)
-      const nextPools = await Promise.all(
-        Object.keys(listPools).map(async (poolAddress) => {
-          const tvl = await getTVL(listPools[poolAddress])
-          return {
-            poolAddress,
-            tvl,
-          }
-        }),
-      )
-      nextPools.sort((poolA, poolB) => poolB.tvl - poolA.tvl)
-      const result = nextPools.map(({ poolAddress }) => poolAddress)
-      return setSortedPools(result)
-    } catch (err) {
-      return setSortedPools(pools)
-    } finally {
-      setLoading(false)
-    }
-  }, [getTVL, listPools, pools])
-
-  useEffect(() => {
-    onSortedPools()
-  }, [onSortedPools])
-
-  if (loading)
-    return (
-      <Row justify="center">
-        <Spin />
-      </Row>
-    )
 
   return (
     <Row gutter={[24, 24]}>
-      {sortedPools.map((poolAddress) => {
+      {Object.keys(listPools).map((poolAddress) => {
         const poolData = listPools[poolAddress]
         if (!poolData) return <Fragment />
         let poolState: PoolState = poolData.state
